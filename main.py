f2292a6e692d3eced29cd1d30da01e7fd76ca54b
hslim8888 <hslim8888@gmail.com>
2023-06-02 10:14:18 +0900

    fetch again

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 def main():
-    sh(f"git tag trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M')}-mattlim origin/main && git push --tags")
+    sh(f"git fetch && git tag trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M')}-mattlim origin/main && git push --tags")
     # sh(f"git tag trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M')}-hslim8888 && git push --tags")
 
 
