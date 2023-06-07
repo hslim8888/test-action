87c85b0924e1436877725e729562097830dadb6c
hslim8888 <hslim8888@gmail.com>
2023-06-07 09:22:21 +0900

    change main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def main():
     tag = f"trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M%S')}-mattlim"
-    sh(f"git fetch && git stash && git checkout main &&  && git tag {tag} origin/main && git push origin {tag}")
+    sh(f"git fetch && git tag {tag} origin/main && git push origin {tag}")
     # sh(f"git tag trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M')}-hslim8888 && git push --tags")
 
 
