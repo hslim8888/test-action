14136dc6f0f9c39f3a5198b5978f0de9b7ec00e8
hslim8888 <hslim8888@gmail.com>
2023-06-12 15:36:32 +0900

    asdf

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from util import sh
 from datetime import datetime
 
 
-def main():
-    tag = f"trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M%S')}-asdf qwer"
-    sh(f"git tag trigger-v{datetime.now().strftime('%Y-%m-%d-%H%M%S')}-{tag} && git push --tags")
+def main(**kwargs):
+    trigger_tag = f'trigger-v{datetime.now().strftime("%Y-%m-%d-%H%M")}'
+    branch = 'main'
+    deploy_message = kwargs.get('m', '')
+    tag = f'-a {trigger_tag} -m "{deploy_message}"' if deploy_message else trigger_tag
+    sh(f'git fetch && git tag {tag} origin/{branch} && git push origin {trigger_tag}')
 
 
 if __name__ == '__main__':
