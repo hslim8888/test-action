580d7e8949799fe8fcc06995eea7f4642e6b0b40
hslim8888 <hslim8888@gmail.com>
2023-06-12 16:08:44 +0900

    parse message of annotated tag

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from util import sh
 from datetime import datetime
 
 
-def main(**kwargs):
+def main(*args, **kwargs):
     trigger_tag = f'trigger-v{datetime.now().strftime("%Y-%m-%d-%H%M")}'
     branch = 'main'
-    deploy_message = kwargs.get('m', '')
+    print(args)
+    print(kwargs)
+    deploy_message = 'test message' #kwargs.get('m', '')
+    print(deploy_message)
     tag = f'-a {trigger_tag} -m "{deploy_message}"' if deploy_message else trigger_tag
-    sh(f'git fetch && git tag {tag} origin/{branch} && git push origin {trigger_tag}')
+    print(tag)
+    # sh(f'git fetch && git tag {tag} origin/{branch} && git push origin {trigger_tag}')
 
 
 if __name__ == '__main__':
