e82ade207bdfa22e53fd771be28220c18cba2d13
hslim8888 <hslim8888@gmail.com>
2023-09-13 11:10:03 +0900

    test backtick `here` and `there`

---
+++ operations.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from pyinfra.operations import python
+
+from util import sh_output, get_commit_hash_and_time_from_tag, sh
+
+
+def create_release_and_notify_slack():
+    new_tag = sh_output('git tag --sort=-creatordate | grep "^v" | sed -n "1p"')
+    new_hash, new_time = get_commit_hash_and_time_from_tag(new_tag)
+    previous_tag = sh_output('git tag --sort=-creatordate | grep "^v" | sed -n "2p"')  # 새로운 tag를 제외한 이전 tag 라 2번째
+    previous_hash, previous_time = get_commit_hash_and_time_from_tag(previous_tag)
+    change_log = sh_output(f'git log --pretty=format:"%h - %s" {previous_hash}..{new_hash}')
+
+    pr_list = sh_output(f'gh pr list --search "is:merged merged:{previous_time}..{new_time}" --json number,title')
+    pr_list = "\n".join([f"#{item['number']} {item['title']}" for item in json.loads(pr_list) if pr_list])
+
+    github_name = sh_output('git config user.name')
+    release_note = f"""[Compare link](https://github.com/mediquitous-dev/zelda/compare/{previous_tag}..{new_tag})
+    <br>
+    <b>PR LIST</b>
+    {pr_list}
+    <br>
+    <b>CHANGE LOG</b>
+    {change_log}
+    """.replace("`", "\`")
+    sh(f'gh release create {new_tag} --title="Release {new_tag}" --notes="{release_note}"')
+
+def tag_latest_commit(new_tag):
+    sh('git fetch')
+    sh(f'git tag {new_tag} origin/main && git push origin {new_tag}')
+
+python.call(_run_once=True,
+            function=tag_latest_commit,
+            new_tag=f'v{datetime.now().strftime("%Y-%m-%d-%H%M%S")}')
+
+
+python.call(_run_once=True, function=create_release_and_notify_slack)
+
--- util.py
+++ util.py
@@ -1,7 +1,26 @@
 import subprocess
+from datetime import datetime
+
+import pytz
 
 
-def sh(command, check=False):
+def sh(command, check=False, capture_output=False):
     print(command)
-    import subprocess
-    subprocess.run(command, shell=True, check=check)
+    return subprocess.run(command, shell=True, check=check, capture_output=capture_output,
+                          text=True if capture_output else False)
+
+
+def sh_output(command, check=False) -> str:
+    return sh(command, check=check, capture_output=True).stdout.strip()
+
+def get_commit_hash_and_time_from_tag(tag):
+    comit_hash = sh_output(f'git show-ref -s {tag}')
+    # {A_commit_time}..{B_commit_time} 을 하면 A_comit_time 포함이기 때문에, 1초를 더해준다.
+    commit_time = sh_output(f'git show -s --format=%cd --date=iso-strict {tag} | tail -n 1')
+    commit_time = datetime.strptime(commit_time, "%Y-%m-%dT%H:%M:%S%z") + timedelta(seconds=1)
+    if commit_time.tzinfo == pytz.utc:
+        commit_time = commit_time.astimezone(pytz.timezone('Asia/Seoul'))
+    commit_time = commit_time.strftime("%Y-%m-%dT%H:%M:%S%z")
+    return comit_hash, commit_time
+
+
