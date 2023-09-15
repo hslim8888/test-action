9853ff37118b76ace4df5962a5cf406efbcc7165
hslim8888 <hslim8888@gmail.com>
2023-09-15 11:45:50 +0900

    pr 용 커밋

--- operations.py
+++ operations.py
@@ -2,6 +2,7 @@ import json
 import textwrap
 from datetime import datetime
 
+from jinja2 import Template
 from pyinfra.operations import python
 
 from util import sh_output, get_commit_hash_and_time_from_tag, sh
@@ -17,22 +18,40 @@ def create_release_and_notify_slack():
     pr_list = sh_output(f'gh pr list --search "is:merged merged:{previous_time}..{new_time}" --json number,title')
     pr_list = "\n".join([f"#{item['number']} {item['title']}" for item in json.loads(pr_list) if pr_list])
 
-    release_note = textwrap.dedent(f"""\
-        [Compare link](https://github.com/mediquitous-dev/zelda/compare/{previous_tag}..{new_tag})
-        <br>
-        <b>PR LIST</b>
-        {pr_list}
-        <br>
-        <b>CHANGE LOG</b>
-        {change_log}
-    """)
+    release_title = f'[Compare link](https://github.com/mediquitous-dev/zelda/compare/{previous_tag}..{new_tag})'
+    slack_title = f'*www.nugu.jp 배포가 완료되었습니다. <https://github.com/mediquitous-dev/zelda/releases/tag/{new_tag}|{new_tag}>*'
+
+    message_template = Template(textwrap.dedent("""\
+        {{ title }}
+        
+        {{ pr_list }}
+        
+        {{ change_log }}
+    """))
+    release_note = message_template.render(title=release_title, pr_list=pr_list, change_log=change_log)
+
+
+
+    print('release_note!!!', release_note)
+    # release_note = textwrap.dedent(f"""\
+    #     [Compare link](https://github.com/mediquitous-dev/zelda/compare/{previous_tag}..{new_tag})
+    #
+    #
+    #     {pr_list}
+    #
+    #
+    #     {change_log}
+    # """)
     sh(f'gh release create {new_tag} --title="Release {new_tag}" --notes="{release_note}"')
 
-    slack_message = textwrap.dedent(f"""\
-        *www.nugu.jp 배포가 완료되었습니다. <https://github.com/mediquitous-dev/zelda/releases/tag/{new_tag}|{new_tag}>*
-
-    """)
+    slack_message = slack_title + '\n\n' + '\n'.join(release_note.splitlines()[1:7])
     print('slack_message!!!', slack_message)
+
+    # slack_message = textwrap.dedent(f"""\
+    #     *www.nugu.jp 배포가 완료되었습니다. <https://github.com/mediquitous-dev/zelda/releases/tag/{new_tag}|{new_tag}>*
+    #
+    # """)
+    # print('slack_message!!!', slack_message)
 
 def tag_latest_commit(new_tag):
     sh('git fetch')
